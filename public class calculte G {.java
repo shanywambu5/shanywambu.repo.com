cbff2d295714d610ad578a9b506f83c1989e4479
shanywambu5 <128737337+shanywambu5@users.noreply.github.com>
2023-03-24 13:04:30 +0300

    Add files via upload

---
+++ public class calculte G {.java
@@ -0,0 +1,26 @@
+public class calculte G {
+    public static void main(string [] arguments){
+        double gravity=-9.81; // earth's gravity in m.s^
+        double fallingTime=30;
+
+        double initialvelocity=0.0;
+        double finalvelocity= gravity*fallingTime+initialvelocity;
+        double initialvelocity=0.0;
+        double finalposition= 0.5*gravtiy *math.pow (falling time,2)+ initialvelocity + fallingTime+initial 
+        system.out.println("the object's position after" +fallingTime+ "second postion"+finalposition+"m.s^");
+        system.out.println("the object'svelocity after"+ fallingTime +"second is" +finalvelocity+"m.s^");
+ 
+   }
+   public static double multiply( double a,double b){
+       return a*b
+       public static double powerasquare (double a){
+           return math.pow(a,2);
+           public static double sum (double a, double b){
+               return a+b;
+               public static void outline(string message ,double value ){
+                   system.out.println(message+""+value);
+               }
+           }
+       }
+   }
+}
